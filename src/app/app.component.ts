b69a350a1d8060b01597afae1c170b54b6c5c3df
dsullivan <drew.sullivan@hyland.com>
2018-05-11 15:35:58 -0400

    First real commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'where-my-car';
+
+  ngOnInit(): void {
+    const marker = document.getElementById('x');
+    marker.style.position = 'absolute';
+    marker.style.left = '0';
+    marker.style.top = '0';
+  }
+
+  save(loc): void {
+    console.log(loc.layerX);
+    console.log(loc.layerY);
+    const marker = document.getElementById('x');
+    const left = loc.layerX;
+    const top = loc.layerY;
+    marker.style.left = `${left - 5}px`;
+    marker.style.top = `${top - 7}px`;
+  }
 }
